716c226437e6e30c129fcb89204231b41868815e
semaraugusto <semaraugusto@dcc.ufmg.br>
2022-02-21 11:08:29 -0300

    begin stow usage, add initial install script, update st

---
+++ install.sh
@@ -0,0 +1,26 @@
+pacman -S base-devel \ # Development
+          vim \
+          git \
+          wget \
+          curl \
+          ripgrep \
+          sxiv
+          rsync \
+          zathura \ 
+          nvidia \ # video-drivers
+          xorg
+          xorg-server \ # xorg stuff
+          xorg-xinit \
+          xmonad-contrib \
+          xmonad \
+          sddm \
+          sddm-kcm \
+          dunst \
+          unclutter \
+          xcompmgr \
+          stow \
+
+
+
+
+cp ./remaps ./setbg ./xcompmgr ./dust ./unclutter ~/.local/bin
